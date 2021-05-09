32f2d0e5323448eb64d06c3f928ec7f6442959ae
twdavidong <64457327+twdavidong@users.noreply.github.com>
2021-05-10 01:57:03 +0800

    Commit 002

--- App.js
+++ App.js
@@ -1,82 +1,108 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { StyleSheet, Text, TouchableOpacity, View, FlatList } from "react-native";
+import React, {useState, useEffect} from "react";
+import { FlatList, StyleSheet, Text, View, Button } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import BlockRGB from "./components/BlockRGB";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
 
 function HomeScreen({navigation}) {
- const [colorArray, setColorArray] = useState([]);
- 
- const numCol = 4;
+    const [colorArray, setColorArray] = useState([]);
+      
+    useEffect(() => {
+      navigation.setOptions({
+        headerRight: () => <Button onPress={addColor} title="Add Colour" />,
+        headerLeft: () => <Button onPress={resetButton} title="Reset" />,
+      });
+    });
 
-function addColor() {
-   setColorArray([
-     ...colorArray,
-     {
-       red: Math.floor(Math.random() * 256),
-       green: Math.floor(Math.random() * 256),
-       blue: Math.floor(Math.random() * 256),
-       id: `${colorArray.length}`,
-     },
-   ]);
- }
-
- function resetButton(){
- setColorArray([]);
- }
-
-
-function renderItem({item}) {
-  return <BlockRGB red={item.red} green={item.green} blue={item.blue}/>;
-}
- 
- return (
-   <View style={styles.container}>
-   <TouchableOpacity 
-        style={{ height:40, justifyContent: "center" }} 
-            onPress={resetButton}
-            >
-      <Text style={{color:"blue"}}>Reset</Text>
-      </TouchableOpacity>
+    function addColor() {
+      setColorArray([
+          ...colorArray,
+          {
+            red: Math.floor(Math.random()* 256),
+            green: Math.floor(Math.random()* 256),
+            blue: Math.floor(Math.random()* 256),
+            id: '${colorArray.length}',
+          },
+      ]);
+    }
 
 
-         <TouchableOpacity 
-        style={{ height:40, justifyContent: "center" }} 
-            onPress={addColor}
-            >
-      <Text style={{color:"red"}}>Add Colour</Text>
-      </TouchableOpacity>
+    return(
+      <View style={styles.container}>
+            <FlatList style={styles.list} data={colorArray} renderItem={renderItem} />    
+      </View>
+  );
 
-     <FlatList style={styles.list} data={colorArray} renderItem={renderItem} numColumns={4} />
-   </View>
- );
+    function renderItem({item})  {
+      return (
+        <TouchableOpacity
+          onPress={() => navigation.navigate("DetailsScreen", { ...item })}
+        >
+          <BlockRGB red={item.red} green={item.green} blue={item.blue} />
+        </TouchableOpacity>
+      );
+    }
+
+    function resetButton(){
+      setColorArray([]);
+    }
 }
 
 function DetailsScreen({ route }) {
- return <Text>Er... more stuff here soon?</Text>;
-}
+  const {red, green, blue} = route.params;
+
+  return (
+    <View
+      style={[
+        styles.container,
+        {backgroundColor: 'rgb(${red}, ${green}, ${blue})'},
+      ]}
+      >
+        <View style={{  padding:  30  }}>
+          <Text style={styles.detailText}>Red: {red}</Text>
+          <Text style={styles.detailText}>Green: {green}</Text>
+          <Text style={styles.detailText}>Blue: {blue}</Text>
+        </View>
+      </View>
+  );
+ }
+
 
 const Stack = createStackNavigator();
 
-export default function App() {
- return (
-   <NavigationContainer>
-     <Stack.Navigator>
-       <Stack.Screen name="Colour List" component={HomeScreen} />
-       <Stack.Screen name="DetailsScreen" component={DetailsScreen} />
-     </Stack.Navigator>
-   </NavigationContainer>
- );
-}
+       export default function App() {
+          return (
+              <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen 
+                        name="Colour List" 
+                        component={HomeScreen}
+                    />
+                    <Stack.Screen 
+                        name="DetailsScreen" 
+                       component={DetailsScreen} 
+                    />
+                    
+                </Stack.Navigator>
+              </NavigationContainer>
+            );
+          }
 
 const styles = StyleSheet.create({
- container: {
-   flex: 1,
-   backgroundColor: "#fff",
-   alignItems: "center",
- },
- list: {
-   width: "100%",
- },
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    list: {
+              width:"100%",
+    },
+    detailText:{
+              fontSize: 24,
+              marginBottom: 20,
+    },      
 });
